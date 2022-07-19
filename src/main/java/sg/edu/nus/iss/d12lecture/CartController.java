0387831fa09377dcafcb565ee578584900cbd167
Tan_Ching_Le <tchinglewow@gmail.com>
2022-07-19 13:32:27 +0800

    Cart controller cart service and item added

---
+++ src/main/java/sg/edu/nus/iss/d12lecture/CartController.java
@@ -0,0 +1,32 @@
+package sg.edu.nus.iss.d12lecture;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sg.edu.nus.iss.d12lecture.CartService;
+
+
+@Controller
+@RequestMapping(path = {"/cart"})
+
+public class CartController {
+
+
+    @GetMapping(produces = { "text/html"})
+    public String displayCart(Model model){
+
+    CartService cs = new CartService();
+        List<Item> cartItems = cs.getShoppingItems();
+        model.addAttribute("cart", cartItems);
+        
+        return "cart";
+    }
+
+
+
+}
+
+
